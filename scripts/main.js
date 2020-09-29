725a1b14d7bb177d82c3498846f84d91b181ea46
EricMTodd <eterictodd@gmail.com>
2020-09-28 19:41:27 -0600

    Initial commit attempting to build a grid without copying code from anywhere. I want to build this with my own skills.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,13 @@
 console.log("Hello world!");
 
-const container = document.getElementById("container");
+function buildBox() {
+  let container = document.createElement("div");
+  container.id = "container";
+  document.body.appendChild(container);
 
-function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    container.appendChild(cell).className = "grid-item";
-  }
+  let box = document.createElement("div");
+  box.id = "box";
+  container.appendChild(box);
 }
 
-makeRows(16, 16);
+buildBox();
