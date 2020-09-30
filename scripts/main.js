18f3cb6a56d929d3cb18fecee2f739feedbcffdd
EricMTodd <eterictodd@gmail.com>
2020-09-29 18:46:42 -0600

    Grid size is now determined dynamically by JavaScript!

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 console.log("Hello world!");
 
-function buildGrid(rows, cols) {
+function buildGrid(gridNum) {
+  let root = document.documentElement;
+  root.style.setProperty("--gridNum", gridNum);
   let container = document.createElement("div");
   container.id = "container";
   document.body.appendChild(container);
-  for (let i = 0; i < rows * cols; i++) {
+  for (let i = 0; i < gridNum * gridNum; i++) {
     let box = document.createElement("div");
     box.id = "box";
-    box.innerHTML = i;
+    box.innerHTML = i + 1;
     container.appendChild(box);
   }
 }
 
-
-buildGrid(16, 16);
+buildGrid(16);
