b0743a40bb9a36bad24d28add021e4a1daf8baa9
Eric Todd <eterictodd@gmail.com>
2020-10-01 22:46:05 -0600

    Minor formatting changes for easier readability.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,22 @@
+function buildGrid(gridNum) {
+  let root = document.documentElement;
+  let container = document.getElementById("container");
+  let grid = document.createElement("div");
+
+  root.style.setProperty("--gridNum", gridNum);
+  grid.id = "grid";
+  container.appendChild(grid);
+
+  for (let i = 0; i < gridNum ** 2; i++) {
+    let box = document.createElement("div");
+
+    box.className = "box";
+    box.id = `box-${[i + 1]}`;
+    box.innerHTML = i + 1;
+    grid.appendChild(box);
+  }
+}
+
 function updateGrid() {
   let grid = document.getElementById("grid");
   let gridNum = document.getElementById("input").value;
@@ -7,23 +26,5 @@ function updateGrid() {
   document.getElementById("input").value = "";
 }
 
-function buildGrid(gridNum) {
-  let root = document.documentElement;
-  let container = document.getElementById("container");
-  let grid = document.createElement("div");
-
-  root.style.setProperty("--gridNum", gridNum);
-  root.style;
-  grid.id = "grid";
-  container.appendChild(grid);
-
-  for (let i = 0; i < gridNum ** 2; i++) {
-    let box = document.createElement("div");
-    box.className = "box";
-    box.id = `box-${[i + 1]}`;
-    box.innerHTML = i + 1;
-    grid.appendChild(box);
-  }
-}
 
 buildGrid(16);
