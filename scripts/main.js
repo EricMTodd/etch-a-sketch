0dbe1a6978eafc8bf3d3b508d6a3583adce68bdf
Eric Todd <eterictodd@gmail.com>
2020-10-05 00:01:36 -0600

    Reconstructing grid based on 2D arrays.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-console.log("sup nerd");
+function buildGrid(rows, cols) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      document.write(`(${i}, ${j}) `)
+    }
+    document.write("<br>")
+  }
+}
+
+buildGrid(5, 5);
